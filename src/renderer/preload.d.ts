7d5fec4d9f5c6b4c877fb56725b7421c584d2704
Travis <Tracisz@hotmail.com>
2022-12-02 14:30:27 +0900

    fixed merge conflict

--- src/renderer/preload.d.ts
+++ src/renderer/preload.d.ts
@@ -3,10 +3,6 @@ import { IPCEvents } from 'main/ipc/types';
 declare global {
   interface Window {
     electron: {
-<<<<<<< HEAD
-      getRelease(arg0: { name: string; payload: undefined; }): void;
-=======
->>>>>>> react_electron
       sendEvent(event: IPCEvents): void;
       on(
         channel: string,
